7dd4c7b97b18eea91cecec6c9948cc2b48a85316
ardiyan15 <ardhiyan15@gmail.com>
2022-05-20 16:59:40 +0700

    update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,15 +1,15 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar custom-background sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
-        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Pengajuan Kredit</span>
+        <img src="{{ asset('dist/img/mandiri-logo-transparent.png') }}" class="center" alt="AdminLTE Logo"
+            style="width: 150px; margin-left: 10%; opacity: .8">
+        {{-- <span class="brand-text font-weight-light">Pengajuan Kredit</span> --}}
     </a>
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-
-                    <a href="{{ route('home') }}" class="nav-link @if ($menu == 'Dashboard') active @endif"><i
+                    <a href="{{ route('home') }}"
+                        class="nav-link @if ($menu == 'Dashboard') active @else custom-color @endif"><i
                             class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -19,7 +19,7 @@
                 <li class="nav-item">
 
                     <a href="{{ route('users.show', Auth::user()->id) }}"
-                        class="nav-link @if ($menu == 'users') active @endif"><i
+                        class="nav-link @if ($menu == 'users') active @else custom-color @endif"><i
                             class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             User Profile
@@ -29,7 +29,7 @@
 
                 @if (Auth::user()->roles == 'mka' || Auth::user()->roles == 'kepala cabang')
                     <li class="nav-item @if ($menu == 'mks') menu-open @endif">
-                        <a href="#" class="@if ($menu == 'mks') active @endif nav-link">
+                        <a href="#" class="@if ($menu == 'mks') active @else custom-color @endif nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 MKA
@@ -40,7 +40,7 @@
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
                                 <a href="{{ route('mks.index') }}"
-                                    class="@if ($sub_menu == 'scoring') active @endif nav-link">
+                                    class="@if ($sub_menu == 'scoring') active @else custom-color @endif nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Skoring MKA</p>
                                 </a>
@@ -51,7 +51,7 @@
 
                 @if (Auth::user()->roles == 'kepala cabang' || Auth::user()->roles == 'mka')
                     <li class="nav-item @if ($menu == 'approval') menu-open @endif">
-                        <a href="#" class="@if ($menu == 'approval') active @endif nav-link">
+                        <a href="#" class="@if ($menu == 'approval') active @else custom-color @endif nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Approval
@@ -62,7 +62,7 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="{{ route('approval.bi_checking') }}"
-                                        class="@if ($sub_menu == 'approval_bi_checking') active @endif nav-link">
+                                        class="@if ($sub_menu == 'approval_bi_checking') active @else custom-color @endif nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Approval BI Checking</p>
                                     </a>
@@ -73,7 +73,7 @@
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
                                     <a href="{{ route('approval.index') }}"
-                                        class="@if ($sub_menu == 'approval_credit') active @endif nav-link">
+                                        class="@if ($sub_menu == 'approval_credit') active @else custom-color @endif nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Approval K. Cabang</p>
                                     </a>
@@ -84,7 +84,7 @@
                 @endif
 
                 <li class="nav-item @if ($menu == 'master') menu-open @endif">
-                    <a href="#" class="@if ($menu == 'master') active @endif nav-link">
+                    <a href="#" class="@if ($menu == 'master') active @else custom-color @endif nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Data Master
@@ -95,14 +95,14 @@
                         @if (Auth::user()->roles == 'superadmin')
                             <li class="nav-item">
                                 <a href="{{ route('employee.index') }}"
-                                    class="@if ($sub_menu == 'employee') active @endif nav-link">
+                                    class="@if ($sub_menu == 'employee') active @else custom-color @endif nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Employee</p>
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a href="{{ route('users.index') }}"
-                                    class="@if ($sub_menu == 'user') active @endif nav-link">
+                                    class="@if ($sub_menu == 'user') active @else custom-color @endif nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>User</p>
                                 </a>
@@ -111,7 +111,7 @@
                         @if (Auth::user()->roles == 'mks' || Auth::user()->roles == 'kepala cabang' || Auth::user()->roles == 'mka')
                             <li class="nav-item">
                                 <a href="{{ route('credits.index') }}"
-                                    class="@if ($sub_menu == 'credit') active @endif nav-link ">
+                                    class="@if ($sub_menu == 'credit') active @else custom-color @endif nav-link ">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Pengajuan Kredit</p>
                                 </a>
@@ -120,7 +120,7 @@
                         @if (Auth::user()->roles == 'superadmin')
                             <li class="nav-item">
                                 <a href="{{ route('sukubunga.index') }}"
-                                    class="@if ($sub_menu == 'suku_bunga') active @endif nav-link ">
+                                    class="@if ($sub_menu == 'suku_bunga') active @else custom-color @endif nav-link ">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Suku Bunga</p>
                                 </a>
@@ -128,32 +128,13 @@
                         @endif
                     </ul>
                 </li>
-
-                {{-- <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Kredit
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-
-                        <li class="nav-item">
-                            <a href="{{ route('credits.index') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Pengajuan Kredit</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li> --}}
                 <hr color="white" width="200px;">
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
                     <li class="nav-item">
                         <button class="btn btn-sm nav-link text-left font-weight-bold text-mute" style="width: 100%;">
-                            <i class="fas fa-sign-out-alt"></i>
-                            <p>
+                            <i class="fas fa-sign-out-alt custom-color"></i>
+                            <p class="custom-color">
                                 Logout
                             </p>
                         </button>
